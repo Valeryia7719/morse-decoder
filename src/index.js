4bf94c164aab83756b4e924f073ad96116e0de81
Valeryia Klopava <bsu.valeria.kl@gmail.com>
2020-10-24 19:40:16 +0300

    init: complete task

--- src/index.js
+++ src/index.js
@@ -38,7 +38,34 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    let result = '';
+    let code = '';
+    let i = 0;
+    while (i < expr.length) {
+        let str = expr.substr(i, 10);
+        for (x = 0; x < str.length; x++) {
+            if (str[x] == '1' && str[x+1] == '0') {
+                code += '.';                
+                x = x + 1;
+            }
+            else if (str[x] == '1' && str[x+1] == '1') {
+                code += '-';
+                x = x + 1;
+            }
+            else if (str[x] == '*') {
+                code += ' ';
+                x = x + 10;
+            } 
+        }
+        if (code == ' ') {result += ' ';}
+        else {
+        result += MORSE_TABLE[code];
+    }
+        console.log(code);
+        code = '';
+        i = i + 10;
+    }
+    return result;
 }
 
 module.exports = {
